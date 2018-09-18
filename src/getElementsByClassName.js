18441ef9d6fd659534908d09bc7a6d6276cfcc62
leemichelle <mlee749@gmail.com>
2018-09-17 20:39:18 -0700

    passed tests

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,53 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  
+  //checks if element has target class and adds it to results
+  var getClassName = function(element) {
+    var classes = element.classList;
+    //not all elements have classes
+    if (classes) {
+      //loop through all classes of element
+      for (var i = 0; i < classes.length; i++) {
+        //check if class matches className
+        if (classes[i] === className) {
+          //if matches, push the entire element into results array
+          result.push(element);
+        }
+      }
+    } 
+  };
+
+  //checks if children have target class
+  var getChildren = function(element) {
+    //creates array of children of element
+    var children = element.childNodes;
+    //loop through array of children
+    for (var i = 0; i < children.length; i++) {
+      //checks if children have target class 
+      getClassName(children[i]);
+      //checks if children's children have target class
+      getChildren(children[i]);
+    }
+  };
+  
+
+  // selects body element
+  var body = document.body;
+  //creates empty array to push matching elements into
+  var result = [];
+  //check if body has target class
+  getClassName(body)
+  //check if body's children have target class
+  getChildren(body);
+  return result;
+  
+
+
+
 };
+
+//You should use document.body, element.childNodes, and element.classList
+
+//make sure to check element, not just the children
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,78 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    console.log('num test')
+    return obj.toString();
+  }
+  if (obj === null) {
+    console.log('test')
+    return '' + null;
+  }
+  if (typeof obj === 'function' || obj === undefined) {
+    return '';
+  }
+  if (Array.isArray(obj)) {
+    if (obj.length === 0 ) {
+      return '[]'
+    }
+    return '[' + obj.map(function(x) {
+      return stringifyJSON(x)
+    })  + ']'
+  }
+  if (typeof obj === 'object') {
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    }
+    var ans = ''
+    for (var key in obj) {
+      if (typeof obj[key] === 'function') {
+        return '{}';
+      }
+      ans += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ','
+    }
+    return '{' + ans.slice(0, ans.length - 1) + '}'
+  }
+
+  
+  
+
+  
+  
+  
 };
+
+// var stringifiableObjects = [
+//   9,
+//   null,
+//   true,
+//   false,
+//   'Hello world',
+//   [],
+//   [8],
+//   ['hi'],
+//   [8, 'hi'],
+//   [1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999],
+//   [8, [[], 3, 4]],
+//   [[[['foo']]]],
+//   {},
+//   {'a': 'apple'},
+//   {'foo': true, 'bar': false, 'baz': null},
+//   {'boolean, true': true, 'boolean, false': false, 'null': null },
+//   // basic nesting
+//   {'a': {'b': 'c'}},
+//   {'a': ['b', 'c']},
+//   [{'a': 'b'}, {'c': 'd'}],
+//   {'a': [], 'c': {}, 'b': true}
+// ];
+
+// unstringifiableValues = [
+//   {f
+//     'functions': function() {},
+//     'undefined': undefined
+//   }
+// ];
